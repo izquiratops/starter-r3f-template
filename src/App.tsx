1b82efe063bc3a9dd5fe78d9a0e8c5882665f339
Jordi <hi@izquiratops.dev>
2022-03-08 12:16:42 +0100

    style changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { Canvas, useFrame } from '@react-three/fiber'
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
+
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
+
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-  // Return the view, these are regular Threejs elements expressed in JSX
+
+    // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
       {...props}
@@ -18,7 +21,7 @@ function Box(props) {
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
+      <boxGeometry args={[1.5, 1.5, 1.5]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   )
